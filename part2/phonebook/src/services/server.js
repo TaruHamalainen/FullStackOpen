c933d1540e4e2f5318907482f7398191c6d22fdf
Taru Hämäläinen <82812247+TaruHamalainen@users.noreply.github.com>
2023-10-17 16:18:00 +0300

    changed server to backend

--- part2/phonebook/src/services/server.js
+++ part2/phonebook/src/services/server.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3001/persons";
+const baseURL = "https://phonebook-2it1.onrender.com/api/persons";
 
 const getAll = () => {
   return axios.get(baseURL);
