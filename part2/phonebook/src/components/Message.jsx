f364bc99283b2e3751df3971fded3a248999779f
Taru Hämäläinen <82812247+TaruHamalainen@users.noreply.github.com>
2023-10-18 11:36:35 +0300

    deleted files

--- part2/phonebook/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Message({ message, messageType }) {
-  return (
-    <div className="bg-orange-400 max-w-lg mx-auto p-4 mb-2 rounded-lg">
-      <p className="text-center text-white">{message}</p>
-    </div>
-  );
-}
